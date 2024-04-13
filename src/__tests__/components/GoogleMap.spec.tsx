277d45dea9d2c79bc8cdb249c48d0d9f811909ef
CharlieGroves <ctrg04@gmail.com>
2024-04-13 17:08:33 +0100

    remove unnecessary mock

--- src/__tests__/components/GoogleMap.spec.tsx
+++ src/__tests__/components/GoogleMap.spec.tsx
@@ -1,11 +1,3 @@
-jest.mock('google-map-react', () => {
-  interface MockGoogleMapReactProps {
-    children: React.ReactNode;
-  };
-  const MockGoogleMapReact = (props: MockGoogleMapReactProps) => <div>{props.children}</div>;
-  return MockGoogleMapReact;
-});
-
 import { render, screen } from "@testing-library/react";
 import GoogleMap from "@/components/GoogleMap";
 
